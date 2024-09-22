890d65b8923ea27be2ad6b3ecf0c2f1003fb6aaf
manojn1106 <manojdeveloper1997@gmail.com>
2024-09-22 21:36:00 +0530

    JS Class2

---
+++ Javascript/Class 2/script.js
@@ -0,0 +1,61 @@
+// JS ----> Functionalities
+
+// Built - in   functions
+
+// alert("hi");
+// console.log("hello");
+
+// User defined functions
+
+// Datatypes:
+
+// a = 10; // a = 10.5;   /// ---> number
+// a = "hi";  /// ---> String
+// a = true;   /// ---> false (Boolean)
+// var a;      /// ---> undefined
+// a = null;
+
+// console.log(typeof a, a);
+
+
+// Variables:
+
+// a = 23;
+// age = 23;
+
+// console.log(age);
+
+// Scoping // Hoisting // Variable Declarations
+
+    // ES  -> ECMA Script
+
+    // ES5 -> Old (2015)
+
+        // var
+
+        // a = 20;
+
+        // // var a = 1000;
+        // // a = 1000;
+
+        // console.log(a);
+        // console.log(a);
+        // console.log(a);
+        // console.log(a);
+        
+    // ES6 -> Latest
+
+        // let
+        
+        let a1;
+
+        a1 = 5000;
+
+        console.log(a1);
+        
+        // const   ---->   Constant
+
+        // const c = 1000;
+
+        // console.log(c);
+        
